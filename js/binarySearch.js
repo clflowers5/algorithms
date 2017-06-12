b6a08176801faf458f324d7f7480edbfa890d444
Chris Flowers <clflowers5@gmail.com>
2017-06-11 22:17:26 -0600

    binary search logs

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -4,9 +4,8 @@ const data = [1, 4, 6, 8, 9, 10, 11, 15, 19, 26, 33];
 function binarySearch(data, target) {
   let mid = Math.floor(data.length / 2);
   let el = data[mid];
-  console.log('eval');
+  console.log('Array state:');
   console.log(data);
-  console.log(mid);
 
   if (el === target) {
     return target;
